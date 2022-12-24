c07eae12054321f5aebe0587123199085d339597
Alla Ponurko <aponurko@gmail.com>
2022-12-24 03:47:11 +0300

    seach

---
+++ ClothingStore/Controllers/Api/Vendors.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ClothingStore.Data;
+using ClothingStore.Models;
+
+namespace ClothingStore.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Vendors : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public Vendors(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Vendors
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Vendor>>> GetVendors()
+        {
+            var query = _context.Vendors.AsQueryable();
+            
+            return await query.ToListAsync();
+            //return await _context.Vendors.ToListAsync();
+        }
+
+        // GET: api/Vendors/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Vendor>> GetVendor(int id)
+        {
+            var vendor = await _context.Vendors.FindAsync(id);
+
+            if (vendor == null)
+            {
+                return NotFound();
+            }
+
+            return vendor;
+        }
+
+        
+        private bool VendorExists(int id)
+        {
+            return _context.Vendors.Any(e => e.Id == id);
+        }
+    }
+}
