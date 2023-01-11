903bbfd4d577c8c5c86dfa6abc8385d454bc4070
Alla Ponurko <aponurko@gmail.com>
2023-01-12 02:00:31 +0300

    img

--- ClothingStore/Models/Product.cs
+++ ClothingStore/Models/Product.cs
@@ -17,22 +17,28 @@ namespace ClothingStore.Models
         public List<Color> colors { get; set; }
         public Color Color { get; set; }
         public int ColorId { get; set; }
+        [Required]
         public Category Category { get; set; }
         public int CategoryId { get; set; }
+        [Required]
         public List<Size> sizes { get; set; }
+        [Required]
         public Size Size { get; set; }
         public int SizeId { get; set; }
         public Sale Sale { get; set; }
+        [Required]
         public Sex Sex { get; set; }
         public int SexId { get; set; }
         public List<Review> reviews { get; set; }
         public Like Like { get; set; }
+        [Required]
         public Vendor Vendor { get; set; }
         [Required]
         public int VendorId { get; set; }
         public List<Status> statuses{ get; set; }
         public int Quantity { get; set; }
         public string Img { get; set; }
+        [Required]
         public Textile Textile { get; set; }
         [Required]
         public int TextileId { get; set; }
