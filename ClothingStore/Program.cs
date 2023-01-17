33dba0ef72ca97fb33b2d6bc70a4a3cd0f02e8b0
Alla Ponurko <aponurko@gmail.com>
2023-01-18 02:44:51 +0300

    chat

--- ClothingStore/Program.cs
+++ ClothingStore/Program.cs
@@ -7,6 +7,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+
+
 namespace ClothingStore
 {
     public class Program
@@ -22,5 +24,6 @@ namespace ClothingStore
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+        
     }
 }
--- ClothingStore/Startup.cs
+++ ClothingStore/Startup.cs
@@ -1,3 +1,4 @@
+using ClothingStore.Chat;
 using ClothingStore.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -8,10 +9,13 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+
+
 
 namespace ClothingStore
 {
@@ -35,11 +39,13 @@ namespace ClothingStore
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -51,6 +57,7 @@ namespace ClothingStore
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
@@ -66,6 +73,11 @@ namespace ClothingStore
                     pattern: "{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapHub<ChatHub>("/chat");
+            });
+           
         }
     }
 }
